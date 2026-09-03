7b15953523af5d76327377a27b05bcfe7218601c
Sagar S <dr.sagars11@gmail.com>
2026-09-03 17:55:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Calculator/calculator.py
+++ Difficulty: Easy/Calculator/calculator.py
@@ -1,6 +1,16 @@
 class Solution:
-    def calculate(self, a: int, b: int, operator: int) -> None:
-        if operator in (1, 2, 3):
-            print((None, a + b, b - a, a * b)[operator], end="")
+    def calculate(self, a: int, b: int, optr: int) -> None:
+        # code here
+        #if optr == 1:
+            #print(a+b)
+        #elif optr == 2:
+            #print(a-b)
+        #elif optr == 3:
+            #print(a*b)
+        #else:
+            #print("Invalid Input")
+        
+        if optr in (1, 2, 3):
+            print((None, a + b, b - a, a * b)[optr], end="")
         else:
             print("Invalid Input", end="")
