7da39e951d7a9a7c46a66e33144ce1f751838b57
Sagar S <dr.sagars11@gmail.com>
2026-09-03 13:41:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check the status - Python/check-the-status-python.py
@@ -0,0 +1,17 @@
+#Back-end complete function Template for Python 3
+
+
+# Function to check value and return accordingly
+class Solution:
+
+    def checkStatus(self, a, b, flag):
+        if ((a >= 0 and b < 0) and flag is False):
+            return True
+
+        if ((a < 0 and b >= 0) and flag is False):
+            return True
+
+        if (a < 0 and b < 0 and flag is True):
+            return True
+
+        return False
